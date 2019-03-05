5127852d8dd0d3829f61900312c6e83400a9cec6
veloso14 <jm.veloso@campus.fct.unl.pt>
2019-03-05 02:17:17 +0000

    Loading Frame

---
+++ app/src/main/java/com/ivisionblog/apps/bottomnavigationdrawer/Loading.java
@@ -0,0 +1,28 @@
+package com.ivisionblog.apps.bottomnavigationdrawer;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class Loading extends AppCompatActivity {
+
+    private static int LOGO_TIME_OUT = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.loading);
+        getSupportActionBar().hide();
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                Intent homeIntent = new Intent(Loading.this, MainActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+
+
+        },LOGO_TIME_OUT);
+    }
+}
