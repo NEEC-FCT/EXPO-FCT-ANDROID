ea0cccc7ac8e9ae7a46a8019f9bf15bbfe91d33b
veloso14 <jm.veloso@campus.fct.unl.pt>
2019-03-08 21:55:49 +0000

    New Map V2

--- app/src/main/java/com/neec/expo/fct/fragments/Map.java
+++ app/src/main/java/com/neec/expo/fct/fragments/Map.java
@@ -5,6 +5,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 import com.neec.expo.fct.R;
 
@@ -16,11 +18,22 @@ public class Map extends Fragment {
     }
 
 
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+
+
+        WebView webView = (WebView) view.findViewById(R.id.webView);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.loadUrl("https://www.google.com/maps/d/viewer?mid=1LC7hMeFmnZ8cj4XGiMAEBam7WHZZIn3k&ll=38.66073135349911%2C-9.205763350000097&z=17");
+
+        return view;
     }
 
 }
