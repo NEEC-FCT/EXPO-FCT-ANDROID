70a36678d3fb48f2e035aeec12bed7087c47c2e9
veloso14 <jm.veloso@campus.fct.unl.pt>
2019-03-16 14:30:01 +0000

    Team Fragment added

---
+++ app/src/main/java/com/neec/expo/fct/fragments/Team.java
@@ -0,0 +1,62 @@
+package com.neec.expo.fct.fragments;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+import com.neec.expo.fct.R;
+import com.neec.expo.fct.semNet;
+
+public class Team extends Fragment {
+
+
+    public Team() {
+        // Required empty public constructor
+    }
+
+    public boolean isInternetAvailable() {
+        ConnectivityManager cm = (ConnectivityManager) getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
+        return cm.getActiveNetworkInfo() != null;
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        if(  !isInternetAvailable() ){
+            Intent intent = new Intent( getContext() ,  semNet.class);
+            startActivity(intent);
+        }
+
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_cursos, container, false);
+
+
+        WebView webView = (WebView) view.findViewById(R.id.webView);
+
+        if (Build.VERSION.SDK_INT >= 19) {
+            webView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+        }
+        else {
+            webView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+        }
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setCacheMode(WebSettings.LOAD_DEFAULT);
+
+        webView.loadUrl("https://expofct.neec-fct.com/equipa/about.html");
+
+        return view;
+    }
+
+}
