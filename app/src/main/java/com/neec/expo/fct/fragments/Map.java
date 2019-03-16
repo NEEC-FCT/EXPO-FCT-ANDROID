b1416693a44ce5d5143e78c9d4d30fa56de970ff
veloso14 <jm.veloso@campus.fct.unl.pt>
2019-03-16 03:20:20 +0000

    FAB ClickListener

--- app/src/main/java/com/neec/expo/fct/fragments/Map.java
+++ app/src/main/java/com/neec/expo/fct/fragments/Map.java
@@ -2,12 +2,14 @@ package com.neec.expo.fct.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
+import com.github.clans.fab.FloatingActionButton;
 import com.neec.expo.fct.R;
 
 public class Map extends Fragment {
@@ -32,6 +34,32 @@ public class Map extends Fragment {
         webSettings.setJavaScriptEnabled(true);
 
         webView.loadUrl("https://www.google.com/maps/d/viewer?mid=1LC7hMeFmnZ8cj4XGiMAEBam7WHZZIn3k&ll=38.66073135349911%2C-9.205763350000097&z=17");
+
+        FloatingActionButton team = (FloatingActionButton) view.findViewById(R.id.team);
+        FloatingActionButton qrcode = (FloatingActionButton) view.findViewById(R.id.qrcode);
+
+
+
+        team.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                Log.d("FAB", "Team");
+
+
+            }
+        });
+
+
+
+        qrcode.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                Log.d("FAB", "QRCODE");
+
+
+            }
+        });
+
 
         return view;
     }
