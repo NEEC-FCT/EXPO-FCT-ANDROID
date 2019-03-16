db04a25abf8fb87502104d79b7df7bf687ef8c92
veloso14 <jm.veloso@campus.fct.unl.pt>
2019-03-16 14:30:27 +0000

    QR Code Reader

---
+++ app/src/main/java/com/neec/expo/fct/QrCodeScanner.java
@@ -0,0 +1,59 @@
+package com.neec.expo.fct;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class QrCodeScanner extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+    private ZXingScannerView mScannerView;
+
+    @Override
+    public void onCreate(Bundle state) {
+        super.onCreate(state);
+        getSupportActionBar().hide();
+        // Programmatically initialize the scanner view
+        mScannerView = new ZXingScannerView(this);
+        // Set the scanner view as the content view
+        setContentView(mScannerView);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // Register ourselves as a handler for scan results.
+        mScannerView.setResultHandler(this);
+        // Start camera on resume
+        mScannerView.startCamera();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        // Stop camera on pause
+        mScannerView.stopCamera();
+    }
+
+    @Override
+    public void handleResult(Result rawResult) {
+        // Do something with the result here
+        // Prints scan results
+        System.out.println("foi enviado: " + rawResult.getText());
+        // Prints the scan format (qrcode, pdf417 etc.)
+        //System.out.println("result: " + rawResult.getBarcodeFormat().toString());
+        //If you would like to resume scanning, call this method below:
+        String url = rawResult.getText();
+
+        if (!url.startsWith("http://") && !url.startsWith("https://"))
+            url = "http://" + url;
+        // Open Website
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        startActivity(browserIntent);
+
+
+    }
+}
